50a6338ce91e82058d406e5252de952700331f6a
lab_virtual <jiajun_xin@outlook.com>
2022-11-16 14:20:24 +0800

    finish multiexpMontgomery

--- src/math/big/mulexp.go
+++ src/math/big/mulexp.go
@@ -1,5 +1,9 @@
 package big
 
+import (
+	"fmt"
+)
+
 // MultiExp sets z1 = x**y1 mod |m|, z2 = x**y2 mod |m| ... (i.e. the sign of m is ignored), and returns z1, z2.
 // If m == nil or m == 0, z = x**y unless y <= 0 then z = 1. If m != 0, y < 0,
 // and x and m are not relatively prime, z is unchanged and nil is returned.
@@ -133,6 +137,7 @@ func multiexpNNMontgomery(x, y1, y2, m nat) []*Int {
 		copy(zz1, RR)
 		RR = zz1
 	}
+
 	// one = 1, with equal length to that of m
 	one := make(nat, numWords)
 	one[0] = 1
@@ -142,70 +147,198 @@ func multiexpNNMontgomery(x, y1, y2, m nat) []*Int {
 	var powers [1 << n]nat
 	powers[0] = powers[0].montgomery(one, RR, m, k0, numWords)
 	powers[1] = powers[1].montgomery(x, RR, m, k0, numWords)
-	for i := 2; i < 1<<n; i++ {
-		powers[i] = powers[i].montgomery(powers[i-1], powers[1], m, k0, numWords)
-	}
 
-	// initialize z1, z2 = 1 (Montgomery 1)
-	var z1, z2, zz2 nat
-	z1 = z1.make(numWords)
-	z2 = z2.make(numWords)
-	copy(z1, powers[0])
-	copy(z2, powers[0])
+	return multimontgomery(RR, m, powers[0], powers[1], k0, numWords, []nat{y1, y2})
+	// for i := 2; i < 1<<n; i++ {
+	// 	powers[i] = powers[i].montgomery(powers[i-1], powers[1], m, k0, numWords)
+	// }
+	// fmt.Println("RR = ", RR.String())
+	// fmt.Println("powers[0] = ", powers[0].String())
+	// fmt.Println("powers[1] = ", powers[1].String())
+	// // initialize z1, z2 = 1 (Montgomery 1)
+	// var z1, z2, zz2, zcom, zzcom nat
+	// z1 = z1.make(numWords)
+	// z2 = z2.make(numWords)
+	// zcom = zcom.make(numWords)
+	// copy(z1, powers[0])
+	// copy(z2, powers[0])
+	// copy(zcom, powers[0])
+	// fmt.Println("z1 = ", z1.String())
 
-	zz1 = zz1.make(numWords)
-	copy(zz2, zz1)
+	// zz1 = zz1.make(numWords)
+	// zz2 = zz2.make(numWords)
+	// zzcom = zzcom.make(numWords)
+	// copy(zz2, zz1)
+	// copy(zzcom, zz1)
 
-	// // same windowed exponent, but with Montgomery multiplications
-	// for i := len(y1) - 1; i >= 0; i-- {
-	// 	yi := y1[i]
-	// 	for j := 0; j < _W; j += n {
-	// 		if i != len(y1)-1 || j != 0 {
-	// 			zz1 = zz1.montgomery(z1, z1, m, k0, numWords)
-	// 			z1 = z1.montgomery(zz1, zz1, m, k0, numWords)
-	// 			zz1 = zz1.montgomery(z1, z1, m, k0, numWords)
-	// 			z1 = z1.montgomery(zz1, zz1, m, k0, numWords)
+	// fmt.Println("test in big.Int")
+	// var squaredPower, temp nat
+	// squaredPower = squaredPower.make(numWords)
+	// temp = temp.make(numWords)
+	// copy(squaredPower, powers[1])
+	// fmt.Println("squaredPower = ", squaredPower.String())
+	// fmt.Println("temp = ", temp.String())
+	// fmt.Println("len(y1) = ", len(y1))
+	// for i := 0; i < len(y1); i++ {
+	// 	for j := 0; j < _W; j++ {
+	// 		mask := Word(1 << j)
+	// 		if (y1[i] & mask) == mask {
+	// 			// montgomery must have the returned value not same as the input values
+	// 			// we have to use this temp as the middle variable
+	// 			temp = temp.montgomery(z1, squaredPower, m, k0, numWords)
+	// 			z1, temp = temp, z1
 	// 		}
-	// 		zz1 = zz1.montgomery(z1, powers[yi>>(_W-n)], m, k0, numWords)
-	// 		z1, zz1 = zz1, z1
-	// 		yi <<= n
+	// 		temp = temp.montgomery(squaredPower, squaredPower, m, k0, numWords)
+	// 		squaredPower, temp = temp, squaredPower
 	// 	}
 	// }
 	// // convert to regular number
-	// zz1 = zz1.montgomery(z1, one, m, k0, numWords)
-	var squaredPower nat
-	copy(squaredPower, powers[1])
-	for i := 0; i < len(y1); i++ {
-		for j := 0; j < _W; j++ {
-			mask := Word(1 << j)
-			if (y1[i] & mask) == mask {
-				z1 = z1.montgomery(z1, squaredPower, m, k0, numWords)
-			}
-			squaredPower = squaredPower.montgomery(squaredPower, squaredPower, m, k0, numWords)
+	// temp = temp.montgomery(z1, one, m, k0, numWords)
+	// z1, temp = temp, z1
+	// fmt.Println("squaredPower = ", squaredPower.String())
+	// fmt.Println("temp = ", temp.String())
+	// fmt.Println("ret1 = ", z1.String())
+	// // One last reduction, just in case.
+	// // See golang.org/issue/13907.
+	// if z1.cmp(m) >= 0 {
+	// 	// Common case is m has high bit set; in that case,
+	// 	// since zz is the same length as m, there can be just
+	// 	// one multiple of m to remove. Just subtract.
+	// 	// We think that the subtract should be sufficient in general,
+	// 	// so do that unconditionally, but double-check,
+	// 	// in case our beliefs are wrong.
+	// 	// The div is not expected to be reached.
+	// 	z1 = z1.sub(z1, m)
+	// 	if z1.cmp(m) >= 0 {
+	// 		_, z1 = nat(nil).div(nil, z1, m)
+	// 	}
+	// }
+	// fmt.Println("ret1 = ", z1.String())
+	// z1.norm()
+	// //zz2.norm()
+	// var ret1, ret2 Int
+	// ret1.abs = z1
+	// ret2.abs = zz2
+	// fmt.Println("ret1 = ", z1.String())
+	// ret1.neg = false
+	// ret2.neg = false
+
+	// return []*Int{&ret1, &ret2}
+}
+
+func multimontgomery(RR, m, power0, power1 nat, k0 Word, numWords int, y []nat) []*Int {
+	ret := make([]*Int, len(y))
+	for i := range ret {
+		ret[i] = new(Int)
+	}
+
+	// initialize each value to be 1 (Montgomery 1)
+	z := make([]nat, len(y))
+	for i := range ret {
+		z[i] = z[i].make(numWords)
+		copy(z[i], power0)
+	}
+
+	fmt.Println("test in big.Int")
+	var squaredPower, temp nat
+	squaredPower = squaredPower.make(numWords)
+	temp = temp.make(numWords)
+	copy(squaredPower, power1)
+	fmt.Println("squaredPower = ", squaredPower.String())
+	fmt.Println("temp = ", temp.String())
+	fmt.Println("len(y[0]) = ", len(y[0]))
+
+	maxLen := 1
+	for i := range y {
+		if len(y[i]) > maxLen {
+			maxLen = len(y[i])
 		}
 	}
 
-	// One last reduction, just in case.
-	// See golang.org/issue/13907.
-	if z1.cmp(m) >= 0 {
-		// Common case is m has high bit set; in that case,
-		// since zz is the same length as m, there can be just
-		// one multiple of m to remove. Just subtract.
-		// We think that the subtract should be sufficient in general,
-		// so do that unconditionally, but double-check,
-		// in case our beliefs are wrong.
-		// The div is not expected to be reached.
-		z1 = z1.sub(z1, m)
-		if z1.cmp(m) >= 0 {
-			_, z1 = nat(nil).div(nil, z1, m)
+	for i := 0; i < maxLen; i++ {
+		for j := 0; j < _W; j++ {
+			mask := Word(1 << j)
+			for k := range y {
+				if len(y[k]) > i {
+					if (y[k][i] & mask) == mask {
+						temp = temp.montgomery(z[k], squaredPower, m, k0, numWords)
+						z[k], temp = temp, z[k]
+					}
+				}
+			}
+			// montgomery must have the returned value not same as the input values
+			// we have to use this temp as the middle variable
+			temp = temp.montgomery(squaredPower, squaredPower, m, k0, numWords)
+			squaredPower, temp = temp, squaredPower
 		}
 	}
-	z1.norm()
-	//zz2.norm()
-	var ret1, ret2 Int
-	ret1.abs = z1
-	ret2.abs = zz2
-	ret1.neg = false
-	ret2.neg = false
-	return []*Int{&ret1, &ret2}
+	// one = 1, with equal length to that of m
+	one := make(nat, numWords)
+	one[0] = 1
+	// convert to regular number
+	for i := range z {
+		temp = temp.montgomery(z[i], one, m, k0, numWords)
+		z[i], temp = temp, z[i]
+	}
+	fmt.Println("squaredPower = ", squaredPower.String())
+	fmt.Println("temp = ", temp.String())
+	for i := range z {
+		// One last reduction, just in case.
+		// See golang.org/issue/13907.
+		if z[i].cmp(m) >= 0 {
+			// Common case is m has high bit set; in that case,
+			// since zz is the same length as m, there can be just
+			// one multiple of m to remove. Just subtract.
+			// We think that the subtract should be sufficient in general,
+			// so do that unconditionally, but double-check,
+			// in case our beliefs are wrong.
+			// The div is not expected to be reached.
+			z[i] = z[i].sub(z[i], m)
+			if z[i].cmp(m) >= 0 {
+				_, z[i] = nat(nil).div(nil, z[i], m)
+			}
+		}
+	}
+
+	// normlize and set value
+	for i := range z {
+		z[i].norm()
+		ret[i].abs = z[i]
+		ret[i].neg = false
+	}
+	return ret
+}
+
+func GCB(a, b nat) nat {
+	var maxBitLen int
+	if len(a) > len(b) {
+		maxBitLen = len(b)
+	} else {
+		maxBitLen = len(a)
+	}
+	fmt.Println("test 0 ")
+	var bitStingsRet nat
+
+	bitStingsRet = bitStingsRet.make(maxBitLen)
+	for i := 0; i < maxBitLen; i++ {
+		bitStingsRet[i] = CommonBits(a[i], b[i])
+		a[i] = a[i] - bitStingsRet[i]
+		b[i] = b[i] - bitStingsRet[i]
+	}
+	return bitStingsRet
+}
+
+func CommonBits(a, b Word) Word {
+	var ret uint
+	ret = 0
+	var mask uint
+	for i := 0; i < 32; i++ {
+		mask = uint(1 << i)
+		if ((uint(a) & mask) == mask) && ((uint(b) & mask) == mask) {
+			//fmt.Println("i == ", i, "mask = ", mask)
+			ret = uint(ret) | mask
+		}
+	}
+
+	return Word(ret)
 }
